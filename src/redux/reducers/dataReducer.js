f22a295090f501f0308027768687b7ba0bfcfab2
Trevor Loula <trevorloula@outlook.com>
2020-08-01 20:10:51 -0500

    Fixed explore loading issues

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -30,7 +30,7 @@ export default function (state = initialState, action) {
       return {
         ...state,
         match: false,
-        profile: action.payload.profile,
+        profile: action.payload,
       };
     case LIKE_USER:
       return {

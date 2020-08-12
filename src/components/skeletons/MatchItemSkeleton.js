b2841e311969c7ef06e96ef3698d5b8b31f861c5
Trevor Loula <trevorloula@outlook.com>
2020-08-11 21:23:00 -0500

    Removed unused import

--- src/components/skeletons/MatchItemSkeleton.js
+++ src/components/skeletons/MatchItemSkeleton.js
@@ -5,7 +5,6 @@ import React from "react";
 import { connect } from "react-redux";
 // MUI stuff
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 import Skeleton from "@material-ui/lab/Skeleton";
